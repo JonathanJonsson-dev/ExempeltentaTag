65be005851030a596c04e5e7c91e97d52db748e6
Jonathan <jojo1130@student.miun.se>
2020-12-29 16:19:49 +0100

    Gjort klart uppgift 2-5

---
+++ Uppgift2/Seat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift2
+{
+    public class Seat
+    {
+        public int SeatNumber { get; set; }
+        public string Type { get; set; }
+        public bool IsTaken { get; set; }
+
+        public override string ToString()
+        {
+            if (IsTaken == true)
+            {
+                return $"Plats {SeatNumber} är upptagen";
+            }
+            else
+            {
+                return $"Plats {SeatNumber} är ledig";
+            }
+            
+        }
+    }
+}
---
+++ Uppgift2/Waggon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift2
+{
+    public class Waggon
+    {
+        public List<Seat> Seats { get; set; }
+
+        public Waggon(int numberOfSeats)
+        {
+            Seats = new List<Seat>(numberOfSeats);
+            InitializeSeats(numberOfSeats);
+        }
+
+        public void InitializeSeats(int numberOfSeats)
+        {
+            
+            for (int i = 0; i < numberOfSeats; i++)
+            {
+                Seat seat = new Seat();
+                seat.SeatNumber = i + 1;
+                Seats.Add(seat);
+            }
+            
+        }
+
+        public void FillSeats()
+        {
+            Random random = new Random();
+            foreach (Seat seat in Seats)
+            {
+                if (random.NextDouble() <= 0.5)
+                {
+                    seat.IsTaken = true;
+                }
+                else
+                {
+                    seat.IsTaken = false;
+                }
+            }
+        }
+
+        
+
+    }
+}
